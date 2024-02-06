536280466070d28c3800e5a608baf0e8a2f8ba8b
Xinyi Ouyang <oyxyddd@gamil.com>
2024-02-05 20:16:17 -0500

    connected to database

---
+++ server.mjs
@@ -0,0 +1,20 @@
+import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+dotenv.config()
+import { error } from "console"
+
+mongoose.connect(process.env.ATLAS_URI)
+
+const db = mongoose.connection
+db.on('error',(error) => console.log(error))
+db.once('open',(error) => console.log("Connected to Database"))
+
+const app = express()
+
+app.use(express.json())
+
+// import restaurantRouter from "./"
+
+// app.listen(3000, () => {console.log('Server Started')})
+
